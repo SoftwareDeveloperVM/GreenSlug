9c2dd3e02ef4b49518e8034ace650ce38575b8d0
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2026-01-17 10:28:51 -0800

    Add NavBarFacts component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
+import NavBarFacts from "./components/NavBarFacts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <NavBarFacts />
         {children} {/* Only pages render here */}
       </body>
     </html>
