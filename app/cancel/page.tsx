1ec2ef2cbd6cac2ff5e02684c38101773b3d2c41
Neal Itharaja <sitharaj@ucsc.edu>
2026-01-17 14:54:44 -0800

    Deleted Stripe files

--- app/cancel/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CancelPage() {
-    return (
-        <div className="p-10">
-            <h1 className="text-3xl font-bold mb-4">Payment Cancelled</h1>
-            <p>Your offer was not completed. You can try again.</p>
-        </div>
-    );
-}
