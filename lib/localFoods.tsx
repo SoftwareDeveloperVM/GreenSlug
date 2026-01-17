f6d27d887d2f1d87a598700298d78b54c5eb1a1b
Neal Itharaja <sitharaj@ucsc.edu>
2026-01-17 14:30:20 -0800

    Created local food data

---
+++ lib/localFoods.tsx
@@ -0,0 +1,20 @@
+export const localFoodData = [
+    {
+        source: "UCSC Dining",
+        localPercent: 62,
+        importedPercent: 38,
+        co2KgPerWeek: 410,
+    },
+    {
+        source: "Santa Cruz Farmers Markets",
+        localPercent: 92,
+        importedPercent: 8,
+        co2KgPerWeek: 90,
+    },
+    {
+        source: "Downtown Restaurants",
+        localPercent: 45,
+        importedPercent: 55,
+        co2KgPerWeek: 530,
+    },
+];
