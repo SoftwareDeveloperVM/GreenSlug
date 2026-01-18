eaa11271249e42143a620f86c22dfc7c864f7022
Neal Itharaja <sitharaj@ucsc.edu>
2026-01-17 23:55:16 -0800

    Final final final try to fix auth0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import "./globals.css";
-
 import { Auth0Provider } from '@auth0/nextjs-auth0/client';
+import React from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <Auth0Provider>
-        <body>
-          {children} {/* Only pages render here */}
-        </body>
-      </Auth0Provider>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <Auth0Provider>
+            <body>
+            {children} {/* Only pages render here */}
+            </body>
+        </Auth0Provider>
+        </html>
+    );
 }
